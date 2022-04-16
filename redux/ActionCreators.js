c146e3154e57998db2db56809c4dd99f9718bc68
Gashaye <gashayem@gmail.com>
2022-04-16 08:26:57 -0400

    Redux Adding Favorites

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -141,3 +141,14 @@ export const addPartners = partners => ({
     type: ActionTypes.ADD_PARTNERS,
     payload: partners
 });
+//Favoites actionCreator
+export const postFavorite = campsiteId => dispatch => {
+    setTimeout(() => {
+        dispatch(addFavorite(campsiteId));
+    }, 2000);
+};
+
+export const addFavorite = campsiteId => ({
+    type: ActionTypes.ADD_FAVORITE,
+    payload: campsiteId
+});
